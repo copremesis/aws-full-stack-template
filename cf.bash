02854350bc7a43ab6a63e2fa02e3e09cdf7e7fba
Robert Ortiz <robert.ortiz@rackspace.com>
2020-04-24 11:54:21 -0500

    adding some notes to the cf.bash ... you need aws cli etc and gotchas

--- cf.bash
+++ cf.bash
@@ -2,4 +2,19 @@
 stack_name=crud
 template="template/master-fullstack.yaml"
 
+
+#https://docs.aws.amazon.com/cli/latest/userguide/install-cliv2-mac.html#cliv2-mac-install-cmd
+#also you will need to sign in using;
+#aws configure
+
+#MRD01YG8WL:aws-full-stack-template robe2809$ aws configure
+#AWS Access Key ID [****************ISV2]:
+#AWS Secret Access Key [****************Lm16]:
+#Default region name [us-west-2]:
+#Default output format [json]:
+
 aws cloudformation deploy --template-file $template --stack-name $stack_name --capabilities CAPABILITY_NAMED_IAM
+
+#to get the apps output url look for outputs in the aws console cloudformation panel for this particular stack
+#Remember to delete your stack ... usage is free but the resources are not ... so be mindful of if your
+#stack is in use ... otherwise you may be billed for resources not in use
